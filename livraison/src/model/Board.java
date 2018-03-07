b8f798ecd9f5f44d643420a4ca85fef6c885a2ae
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-03-07 17:04:43 +0100

    création de grille

--- livraison/src/model/Board.java
+++ livraison/src/model/Board.java
@@ -1,20 +1,45 @@
 package model;
 
 public class Board {
-    
+
     private int width;
     private int height;
-    
+		public Tile[][] grid;
+
     public Board(int width, int height) {
         this.width = width;
         this.height = height;
     }
-    
+
     public int getWidth() {
         return this.width;
     }
-    
+
     public int getHeight() {
         return this.height;
     }
+
+		public void createGrid() {
+			this.grid=new Tile[width][height];
+			for (int i=0; i==this.width-1;i++){
+				for (int j=0; j==this.height-1;j++){
+					if (i*j<=width*height-1){
+						this.grid[i][j]=new FullTile(i,j,i*width+j);
+					}else{
+						this.grid[i][j]=new EmptyTile(i,j);
+					}
+				}
+			}
+		}
+
+		public String toString() {
+			String ch="";
+			for (int i=0; i==this.width-1;i++){
+				for (int j=0; j==this.height-1;j++) {
+					ch+=this.grid[i][j].toString() + " ";
+				}
+					ch+="\n";
+			}
+			return ch;
+		}
 }
--- livraison/src/model/Main.java
+++ livraison/src/model/Main.java
@@ -1,7 +1,11 @@
 package model;
 
 public class Main {
-    public static void main(String[] args) {
-        
-    }
+
+	public static void main(String[] args) {
+		Board test= new Board(3,3);
+		test.createGrid();
+		System.out.println(test.grid);
+		System.out.println(test);
+	}
 }

e625a6f8b194362561ddb4accd08f49d2a4eb3f0
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-04-01 17:57:22 +0200

    Simplification de code

--- livraison/src/model/Main.java
+++ livraison/src/model/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 		boolean exit = false;
 		long start= System.currentTimeMillis();
-		Board mystic_square= new Board(4,4);
+		Board mystic_square= new Board(3,3);
 		mystic_square.shuffle(10000);
 		while (!(mystic_square.isSolved())) {
 			Scanner sc= new Scanner(System.in);
@@ -22,16 +22,16 @@ public class Main {
 			System.out.println("Choisissez un déplacement: z, q, s, d   Résolution automatique : r   Quitter : e");
 			String selectMove = sc.nextLine();
 
-			if (selectMove.equals("z") && (mystic_square.neighbours(empty.getX(),empty.getY()).contains(Board.Direction.UP))) {
+			if (selectMove.equals("z") && (possible_moves.contains(Board.Direction.UP))) {
 				mystic_square.move(Board.Direction.UP);
 			}
-			if (selectMove.equals("q") && (mystic_square.neighbours(empty.getX(),empty.getY()).contains(Board.Direction.LEFT))) {
+			if (selectMove.equals("q") && (possible_moves.contains(Board.Direction.LEFT))) {
 				mystic_square.move(Board.Direction.LEFT);
 			}
-			if (selectMove.equals("s") && (mystic_square.neighbours(empty.getX(),empty.getY()).contains(Board.Direction.DOWN))) {
+			if (selectMove.equals("s") && (possible_moves.contains(Board.Direction.DOWN))) {
 				mystic_square.move(Board.Direction.DOWN);
 			}
-			if (selectMove.equals("d") && (mystic_square.neighbours(empty.getX(),empty.getY()).contains(Board.Direction.RIGHT))) {
+			if (selectMove.equals("d") && (possible_moves.contains(Board.Direction.RIGHT))) {
 				mystic_square.move(Board.Direction.RIGHT);
 			}
 			if (selectMove.equals("r"))  {
